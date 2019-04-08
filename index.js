feabd2e59e589e1efadec321b2a5029a8ea5e8b8
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-04-08 17:46:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.once('ready', () => {
 });
 client.on('message', message => {
 	if (message.content === 'c!about') {
-		message.channel.send('This Bot Was For Coding Lounge And Will Be Helping U With Ur Coding.         Thank For Choosing Coding Bot#4060 Have A Nice Day');
+		message.channel.send(' Hello! This Bot Was Made For Coding Lounge And Will Be Helping U With Ur Coding Lesson.Thank For Choosing Coding Bot#4060 Have A Nice Day');
 	}
 });
 
